fb916990d6b27c0f724613cd086876311c8a63da
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-09-26 17:47:15 +0700

    fix loi thanh search bar va the div chua image user dinh vao nhau

--- book-store-mern-project/frontend/src/App.jsx
+++ book-store-mern-project/frontend/src/App.jsx
@@ -4,13 +4,13 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
       <Navbar />
-      <main className="flex-1 max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
-      <footer className="mt-auto">Footer</footer>
-    </div>
+      <footer>Footer day ne</footer>
+    </>
   );
 }
 
